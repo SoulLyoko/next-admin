8b95323f8de2e5869c3186e730fd3095ac65ee8f
Lyoko <519052532@qq.com>
2025-06-12 15:22:38 +0800

    feat: oxlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,10 @@
 import antfu from '@antfu/eslint-config'
+import oxlint from 'eslint-plugin-oxlint'
 
 export default antfu(
   {
     unocss: true,
     formatters: true,
   },
-  {
-    rules: {
-      'ts/ban-ts-comment': 'off',
-    },
-  },
+  oxlint.configs['flat/all'],
 )
